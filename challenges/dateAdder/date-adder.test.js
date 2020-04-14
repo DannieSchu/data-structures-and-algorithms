9d9e7564bcd7d341bb1c0f658b44aa54c28214d2
Dannie Schumaker <draeschumaker@gmail.com>
2020-04-13 22:36:55 -0700

    write failing test for seconds

--- challenges/dateAdder/date-adder.test.js
+++ challenges/dateAdder/date-adder.test.js
@@ -1,10 +1,17 @@
 const { add } = require('./date-adder.js');
 
 describe('date adder', () => {
-  it('adds a time to the date', () => {
+  it('adds an hour to the date', () => {
     const date = new Date('1987-08-13T06:15:20.135Z');
     const diff = '10h';
 
     expect(add(date, diff)).toEqual(new Date ('1987-08-13T16:15:20.135Z'));
+  });
+
+  it('adds seconds to the date', () => {
+    const date = new Date('1987-08-13T06:15:20.135Z');
+    const diff = '25s';
+
+    expect(add(date, diff)).toEqual(new Date ('1987-08-13T06:15:45.135Z'));
   });
 });
