8eb06e9d84ac0bd58717b26b2a1c9fe4e6dfeec1
Dannie Schumaker <draeschumaker@gmail.com>
2020-04-13 20:56:35 -0700

    formulate plan

---
+++ challenges/dateAdder/date-adder-test.js
@@ -0,0 +1,7 @@
+const { add } = require('./date-adder');
+
+describe('addition of date', () => {
+  const date = new Date();
+
+
+})
---
+++ challenges/dateAdder/date-adder.js
@@ -0,0 +1,24 @@
+/*
+CHALLENGE 
+date is a Date object.
+diff is a string representing the amount of time to add to date
+diff is a number followed by an operator
+e.g. 10d would add ten days
+*/
+
+
+const add = (date, diff) => {
+  // separate 'diff' string into array containing two strings
+  //    const [val, operator] = diff; 
+  // turn val into number
+  // modify date conditionally
+  //  if(operator === h) {
+  //    date.setHours(date.getHours() + val)
+  // }
+
+  // return new date with diff added to original date
+
+};
+
+module.exports = { add };
+
