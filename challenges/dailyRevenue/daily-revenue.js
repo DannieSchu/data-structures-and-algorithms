6aa245424a3f54481e31cf1b12258b8116d1acbc
Dannie Schumaker <draeschumaker@gmail.com>
2020-04-14 21:15:18 -0700

    write out plan

---
+++ challenges/dailyRevenue/daily-revenue.js
@@ -0,0 +1,12 @@
+module.exports = (timestamps) => {
+  // iterate over each item in the timestamps array
+  // convert item's timestamp property to date string
+  // const date = new Date(object.timestamp).toDateString()
+
+  // use reduce to return object of correct shape
+  // find out if there is any revenue for a certain date (does accumulator have any revenue for a given date?)
+  // if there is revenue, add value of price property to it
+  // otherwise, set accumulator equal to price
+
+  // sort?
+};
