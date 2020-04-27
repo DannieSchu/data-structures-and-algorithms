d80eaa9d41635d316218322d80dfa18477fdd27b
Dannie Schumaker <draeschumaker@gmail.com>
2020-04-27 00:47:40 -0700

    create, update and return new date instead of modifying original

--- challenges/dateAdder/date-adder.js
+++ challenges/dateAdder/date-adder.js
@@ -1,16 +1,17 @@
 module.exports = (date, diff) => {
+  const newDate = new Date(date);
   const operator = diff.charAt(diff.length - 1);
   const val = Number(diff.substring(0, diff.length - 1));
 
-  if(operator === 's') date.setSeconds(date.getSeconds() + val);
-  else if(operator === 'm') date.setMinutes(date.getMinutes() + val);
-  else if(operator === 'h') date.setHours(date.getHours() + val);
-  else if(operator === 'd') date.setDate(date.getDate() + val);
-  else if(operator === 'w') date.setDate(date.getDate() + (7 * val));
-  else if(operator === 'M') date.setMonth(date.getMonth() + val);
-  else if(operator === 'y') date.setFullYear(date.getFullYear() + val);
+  if(operator === 's') newDate.setSeconds(date.getSeconds() + val);
+  else if(operator === 'm') newDate.setMinutes(date.getMinutes() + val);
+  else if(operator === 'h') newDate.setHours(date.getHours() + val);
+  else if(operator === 'd') newDate.setDate(date.getDate() + val);
+  else if(operator === 'w') newDate.setDate(date.getDate() + (7 * val));
+  else if(operator === 'M') newDate.setMonth(date.getMonth() + val);
+  else if(operator === 'y') newDate.setFullYear(date.getFullYear() + val);
   else {
     throw new Error('Please enter a valid operator.');
   }
-  return date;
+  return newDate;
 };
